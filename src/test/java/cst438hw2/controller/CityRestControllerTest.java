ace7af86e09639400bda32bf923099fa411e2628
jpettit <jpettit@csumb.edu>
2020-07-01 17:26:20 -0700

    Updates to Project Setup

--- src/test/java/cst438hw2/controller/CityRestControllerTest.java
+++ src/test/java/cst438hw2/controller/CityRestControllerTest.java
@@ -2,24 +2,14 @@ package cst438hw2.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import cst438hw2.domain.*;
 import cst438hw2.service.CityService;
 
