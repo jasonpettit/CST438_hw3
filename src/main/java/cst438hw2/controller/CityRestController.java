dc7e655a3e8a87cca06d95105c634ec9f440172f
jpettit <jpettit@csumb.edu>
2020-07-07 13:36:05 -0700

    CityService Test Working

--- src/main/java/cst438hw2/controller/CityRestController.java
+++ src/main/java/cst438hw2/controller/CityRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,16 +27,15 @@ public class CityRestController {
   @GetMapping("/api/cities/{city}")
   public ResponseEntity<CityInfo> getWeather(@PathVariable("city") String cityName) {
 
+    // lookup cities in the database that match the city
     List<City> cities = cityRepository.findByName(cityName);
     if (cities.size() == 0) {
       return new ResponseEntity<CityInfo>(HttpStatus.NOT_FOUND); //city name not found send 404
     }
     else {
       City currentCity = cities.get(0); //in case of multiple cities, take the first one
-
-      CityInfo restCity = cityService.getCityInfo(currentCity.getName());
-
-      return new ResponseEntity<CityInfo>(restCity, HttpStatus.OK);
+      CityInfo restCity = cityService.getCityInfo(currentCity.getName()); // get city info
+      return new ResponseEntity<CityInfo>(restCity, HttpStatus.OK); // send 200 and JSON city info
     }
   }
 }
--- src/test/java/cst438hw2/service/CityServiceTest.java
+++ src/test/java/cst438hw2/service/CityServiceTest.java
@@ -1,20 +1,13 @@
 package cst438hw2.service;
  
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-
-import java.text.SimpleDateFormat;
-
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
-
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import static org.mockito.ArgumentMatchers.anyString;
 
 import cst438hw2.domain.*;
  
@@ -33,26 +26,124 @@ public class CityServiceTest {
 	@MockBean
 	private CountryRepository countryRepository;
 
-	
 	@Test
 	public void contextLoads() {
 	}
 
-
 	@Test
 	public void testCityFound() throws Exception {
-		// TODO 
+		// test definition of country, city, and cities list
+		Country country = new Country("TST", "TestCountry");
+		City city = new City(1, "TestCity", "TST", "DistrictTest", 100000);
+		List<City> cities = new ArrayList<City>();
+		cities.add(city);
+
+		// Test WeatherService
+		// create the stub calls and return data for weatherService
+		// when the getTempAndTime is called with the name parameter "TestCity",
+		// the stub will return the given temp and time
+		given(weatherService.getTempAndTime("TestCity"))
+				.willReturn(new TempAndTime(123.45, 1234560000, -800));
+
+		// Test CityRepository database
+		// create the stub calls and return data for cityRepository
+		// when the findByName is called with the name parameter "TestCity"
+		// the stub will return a list of cities
+		given(cityRepository.findByName("TestCity")).willReturn(cities);
+
+		// Test CountryRepository database
+		// create the stub calls and return data for countryRepository
+		// when the findByCode is called the name parameter "TST"
+		// the stub will return the TestCountry associated with country code TST
+		given(countryRepository.findByCode("TST")).willReturn(country);
+
+		// perform the tests
+		CityInfo cityResult = cityService.getCityInfo("TestCity");
+		CityInfo expectedResult = new CityInfo(1, "TestCity", "TST", "TestCountry", "DistrictTest",
+				100000, 76, "13:16");
+
+		// assertions
+		assertThat(cityResult).isEqualTo(expectedResult);
 	}
 	
 	@Test 
 	public void  testCityNotFound() {
-		// TODO 
+		// test definition of country, city, and cities list
+		Country country = new Country("TST", "TestCountry");
+		City city = new City(1, "TestCity", "TST", "DistrictTest", 100000);
+		List<City> cities = new ArrayList<City>();
+		cities.add(city);
+
+		// Test WeatherService
+		// create the stub calls and return data for weatherService
+		// when the getTempAndTime is called with the name parameter "TestCity",
+		// the stub will return the given temp and time
+		given(weatherService.getTempAndTime("TestCity"))
+				.willReturn(new TempAndTime(123.45, 1234560000, -800));
+
+		// Test CityRepository database
+		// create the stub calls and return data for cityRepository
+		// when the findByName is called with the name parameter "TestCity"
+		// the stub will return a list of cities
+		given(cityRepository.findByName("TestCity")).willReturn(cities);
+
+		// Test CountryRepository database
+		// create the stub calls and return data for countryRepository
+		// when the findByCode is called the name parameter "TST"
+		// the stub will return the TestCountry associated with country code TST
+		given(countryRepository.findByCode("TST")).willReturn(country);
+
+		// perform the tests
+		CityInfo cityResult = cityService.getCityInfo("BadTest");
+		CityInfo expectedResult = null;
+
+		// assertions
+		assertThat(cityResult).isEqualTo(expectedResult);
 	}
 	
 	@Test 
 	public void  testCityMultiple() {
-		// TODO 
-		
-	}
+		// test definition of multiple countries, cities, and cities lists
+		Country country0 = new Country("TST0", "TestCountry0");
+		Country country1 = new Country("TST1", "TestCountry1");
+		Country country2 = new Country("TST2", "TestCountry2");
 
+		City city0 = new City(1, "TestCity", "TST0", "DistrictTest0", 100000);
+		City city1 = new City(1, "TestCity", "TST1", "DistrictTest1", 111111);
+		City city2 = new City(1, "TestCity", "TST2", "DistrictTest2", 222222);
+
+		List<City> cities = new ArrayList<City>();
+		cities.add(city0);
+		cities.add(city1);
+		cities.add(city2);
+
+		// Test WeatherService
+		// create the stub calls and return data for weatherService
+		// when the getTempAndTime is called with the name parameter "TestCity",
+		// the stub will return the given temp and time
+		given(weatherService.getTempAndTime("TestCity"))
+				.willReturn(new TempAndTime(123.45, 1234560000, -800));
+
+		// Test CityRepository database
+		// create the stub calls and return data for cityRepository
+		// when the findByName is called with the name parameter "TestCity"
+		// the stub will return a list of cities
+		given(cityRepository.findByName("TestCity")).willReturn(cities);
+
+		// Test CountryRepository database
+		// create the stub calls and return data for countryRepository
+		// when the findByCode is called the name parameter "TST"
+		// the stub will return the TestCountry associated with country code TST
+		given(countryRepository.findByCode("TST0")).willReturn(country0);
+		given(countryRepository.findByCode("TST1")).willReturn(country1);
+		given(countryRepository.findByCode("TST2")).willReturn(country2);
+
+		// perform the tests
+		CityInfo cityResult = cityService.getCityInfo("TestCity");
+		CityInfo expectedResult = new CityInfo(1, "TestCity", "TST", "TestCountry", "DistrictTest",
+				100000, 76, "13:16");
+
+		// assertions
+		assertThat(cityResult).isEqualTo(expectedResult);
+	}
 }
